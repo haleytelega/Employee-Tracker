eee08b83baebb7f29f92daa6ce2e6601e246b3c6
Haley Telega <haleytelega@outlook.com>
2022-05-08 14:26:56 -0400

    trying to insert valuesinto employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,4 +19,16 @@ VALUES
     ('Finance Analyst', 100000, 6),
     ('I.T. Manager', 150000, 2);
 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ('Willow', 'Guerra', 1),
+    ('Eliana', 'Montgomery', 2),
+    ('London', 'Vazquez', 3),
+    ('Sterling', 'Hayden', 4),
+    ('Finley', 'Norton', 5),
+    ('Julius', 'Woods', 6),
+    ('Katherine', 'Pearce', 7),
+    ('Alison', 'Mooney', 8),
+    ('Carter', 'Brown', 9),
+    ('Melanie', 'Clarke', 10);
 
