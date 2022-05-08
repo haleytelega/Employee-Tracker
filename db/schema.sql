f7ba2b63151ad626c39e860ac68e67e69e4a2184
Haley Telega <haleytelega@outlook.com>
2022-05-08 13:37:33 -0400

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS employee;
+
+DROP TABLE IF EXISTS role;
+
+DROP TABLE IF EXISTS department;
+
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_employee FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
