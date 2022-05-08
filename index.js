b097d123b22843af86d9759f8f1dd016fb54d281
Haley Telega <haleytelega@outlook.com>
2022-05-08 12:43:15 -0400

    added questions

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+
+const questions = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'allQuestions',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee',
+        'Update an employee role']
+        }
+    ])
+};
+
+questions();
