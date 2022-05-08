6501987d30a13afbd10cbb537ea6301ab91c0d44
Haley Telega <haleytelega@outlook.com>
2022-05-08 14:00:59 -0400

    Added role values

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,17 @@ VALUES
     ('Service'),
     ('Billing'),
     ('Finance');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Recrutor', 60000, 1),
+    ('Sales Representative', 40000, 3),
+    ('Service Representative', 50000, 4),
+    ('Front line', 45000, 2),
+    ('HR Director', 100000, 1),
+    ('Billing Administrator', 55000, 5),
+    ('Sales Executive', 200000, 3),
+    ('Finance Analyst', 100000, 6),
+    ('I.T. Manager', 150000, 2);
+
+
